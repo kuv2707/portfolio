6db62a31c3a16f12a41f4063d28a877413a0c02d
kuv2707 <kislayuv27@gmail.com>
2023-06-26 11:34:46 +0530

    secured auth

--- scripts/spotify_alt.js
+++ scripts/spotify_alt.js
@@ -27,26 +27,121 @@ async function generateCodeChallenge(codeVerifier)
 }
 
 const clientId = "5fe2644f61214899bdf2b0dc2f20234a"
-const redirectUri = "localhost:5500/thing.html"
-let codeVerifier = generateRandomString(128);
+const redirectUri = "https://kuv2707.github.io/portfolio/"
 
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
 
-generateCodeChallenge(codeVerifier).then(codeChallenge =>
+if(!code)
 {
-    let state = generateRandomString(16);
-    let scope = 'user-read-private user-read-email';
-
+    let codeVerifier = generateRandomString(128);
     localStorage.setItem('code_verifier', codeVerifier);
+    
+    generateCodeChallenge(codeVerifier).then(codeChallenge =>
+    {
+        let state = generateRandomString(16);
+        let scope = 'user-read-private user-read-email';
+    
+        localStorage.setItem('code_verifier', codeVerifier);
+    
+        let args = new URLSearchParams({
+            response_type: 'code',
+            client_id: clientId,
+            scope: scope,
+            redirect_uri: redirectUri,
+            state: state,
+            code_challenge_method: 'S256',
+            code_challenge: codeChallenge
+        });
+    
+        window.location = 'https://accounts.spotify.com/authorize?' + args;
+    })
 
+}
+else
+{
+    console.log("got code", code)
+    let codeVerifier = localStorage.getItem('code_verifier');
     let args = new URLSearchParams({
-        response_type: 'code',
-        client_id: clientId,
-        scope: scope,
+        grant_type: 'authorization_code',
+        code: code,
         redirect_uri: redirectUri,
-        state: state,
-        code_challenge_method: 'S256',
-        code_challenge: codeChallenge
+        code_verifier: codeVerifier,
+        client_id: clientId
+    });
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: args
+    }).then(res => res.json())
+    .then(data => {
+        // fetchProfile(data.access_token).then(profile => console.log(profile))
+        getAlbum(data)
+        getShow(data)
+    })
+
+}
+
+async function fetchProfile(token) {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
 
-    window.location = 'https://accounts.spotify.com/authorize?' + args;
-})
+    return await result.json();
+}
+
+
+
+let albs=["1BUb9ayIoyy6T3iwMscDts","43mAHKPa4iB2er88lxD9Q8"]
+let alb=albs[Math.floor(Math.random()*albs.length)]
+
+function getAlbum(data)
+{
+  fetch("https://api.spotify.com/v1/albums/"+alb,{
+    authorization:"Bearer "+data.access_token,
+    method:"GET",
+    headers:{
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer '+data.access_token
+      },
+    
+  }).then(res=>res.json())
+  .then(data=>{
+    let k=document.querySelector("#link_album")
+    k.querySelector("#title").innerHTML=data.name
+    console.log(data.artists)
+    k.querySelector("#artist").innerHTML=data.artists.map(a=>a.name).join(", ")
+    k.querySelector("#cover").src=data.images[0].url
+    k.querySelector("#cover").style.width="300px"
+    k.querySelector("#cover").style.height="300px"
+    k.querySelector("#cover").style.borderRadius="20px"
+    k.href=data.external_urls.spotify
+
+  })
+}
+
+let shows=["00h8GrMQbmSAfiNWYPLvhx","0sGGLIDnnijRPLef7InllD"]
+function getShow(data)
+{
+    fetch("https://api.spotify.com/v1/shows/"+shows[Math.floor(Math.random()*shows.length)]+"?market=IN",{
+    authorization:"Bearer "+data.access_token,
+    method:"GET",
+    headers:{
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer '+data.access_token
+      },
+    
+  }).then(res=>res.json()).then(data=>{
+    let k=document.querySelector("#link_show")
+    k.querySelector("#title").innerHTML=data.name
+    console.log(data)
+    k.querySelector("#artist").innerHTML=data.publisher
+    k.querySelector("#cover").src=data.images[0].url
+    k.querySelector("#cover").style.width="300px"
+    k.querySelector("#cover").style.height="300px"
+    k.querySelector("#cover").style.borderRadius="20px"
+    k.href=data.external_urls.spotify
+    })
+}
